bb8145cd7272ae2af493e555deb10f9fea7edc78
AlexanderPuckhaber <alexpuckhaber@gmail.com>
2022-07-25 15:44:15 -0400

    cleanup

--- Makefile
+++
@@ -1,4 +0,0 @@
-SRCS=perf_profiler_wrapper_test.cc perf_profiler.cc perf_profiler.h matmul.cc matmul.h
-
-perf-profiler-wrapper-test: $(SRCS)
-	g++ -Wall -I /usr/local/include -L /usr/local/lib $(SRCS) -o perf_profiler_wrapper_test -g -l:libpfm.a
--- matmul.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include <inttypes.h>
-
-void init_2d_vector(std::vector<float>& V, int I, int J, float default_value = 0);
-
-void matmul_naive(std::vector<std::vector<float>>& A, std::vector<std::vector<float>>& B, std::vector<std::vector<float>>& C);
-
-void matmul_tiled(std::vector<float>& A, std::vector<float>& B, std::vector<float>& C, int I, int J, int K, int block_size);
