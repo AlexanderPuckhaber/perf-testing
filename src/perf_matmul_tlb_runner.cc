0da337a372c40dbeb4a44af92209e56060d34cee
AlexanderPuckhaber <alexpuckhaber@gmail.com>
2022-09-22 14:39:50 -0400

    change randomization to cache-line granularity

--- src/perf_matmul_tlb_runner.cc
+++ src/perf_matmul_tlb_runner.cc
@@ -10,6 +10,62 @@
 #include <iterator>
 
 #define PAGE_SIZE 4096
+#define CACHE_LINE 64
+
+void randomly_swap_cache_blocks_across_pages(std::vector<uint32_t>& index_lut, float frac_to_randomly_swap) {
+  uint32_t lut_num_pages = index_lut.size() / PAGE_SIZE;
+
+  uint32_t idx_per_cache_line = CACHE_LINE / sizeof(float);
+
+  std::vector<uint32_t> lut_cache_lines(index_lut.size() / idx_per_cache_line);
+
+  for (unsigned int i = 0; i < lut_cache_lines.size(); i++) {
+    lut_cache_lines[i] = i * idx_per_cache_line;
+  }
+
+  unsigned int num_randomized;
+
+  if (frac_to_randomly_swap == 0.0) {
+    num_randomized = 0;
+  } else {
+    num_randomized = (int) (((float) lut_cache_lines.size()) * frac_to_randomly_swap);
+  }
+
+  // printf("frac_randomized: %f, num_randomized: %d\n", frac_randomized, num_randomized);
+
+  // num_randomized = 50000;
+
+  std::vector<uint32_t> idx_to_randomize;
+
+  std::sample(lut_cache_lines.begin(), lut_cache_lines.end(), std::back_inserter(idx_to_randomize), num_randomized, std::mt19937{std::random_device{}()});
+
+  for (uint32_t idx : idx_to_randomize) {
+    uint32_t page_num, page_offset;
+    page_num = idx / PAGE_SIZE;
+    page_offset = idx % PAGE_SIZE;
+    
+
+    uint32_t rand_target_page_num = rand() % lut_num_pages;
+
+    for (uint32_t j = 0; j < idx_per_cache_line; j++) {
+      uint32_t target_idx = rand_target_page_num * (PAGE_SIZE / sizeof(float)) + page_offset + j;
+
+      // printf("idx: %d, page_num: %d, page_offset: %d target_page_num: %d target_idx: %d\n", idx, page_num, page_offset, rand_target_page_num, target_idx);
+
+      uint32_t tmp = 0;
+
+      if (target_idx > index_lut.size()) {
+        // printf("bad idx!\n");
+        // exit(1);
+      } else {
+
+        tmp = index_lut[target_idx];
+        index_lut[target_idx] = index_lut[idx];
+        index_lut[idx] = tmp;
+      }
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   int c;
@@ -59,62 +115,11 @@ int main(int argc, char *argv[]) {
   uint32_t B_num_pages = (J * K * sizeof(float)) / PAGE_SIZE;
   uint32_t C_num_pages = (I * J * sizeof(float)) / PAGE_SIZE;
 
-  // printf("A_num_pages: %d, B_num_pages: %d\n", A_num_pages, B_num_pages);
-  // printf("C_num_pages: %d\n", C_num_pages);
 
   std::vector<uint32_t> A_index_lut(I*K);
 
-  // std::string in = "abcdefgh", out;
-  //   std::sample(A_index_lut.begin(), A_index_lut.end(), std::back_inserter(out),
-  //               5, std::mt19937{std::random_device{}()});
-  //   std::cout << "five random letters out of " << in << " : " << out << '\n';
-
-  //   while (1) {};
-
   for (unsigned int i = 0; i < I*K; i++) {
     A_index_lut[i] = i;
-    // printf("%d, ", A_index_lut[i]);
-  }
-
-  unsigned int num_randomized;
-
-  if (frac_randomized == 0.0) {
-    num_randomized = 0;
-  } else {
-    num_randomized = (int) (((float) I*K) * frac_randomized);
-  }
-
-  // printf("frac_randomized: %f, num_randomized: %d\n", frac_randomized, num_randomized);
-
-  // num_randomized = 50000;
-
-  std::vector<uint32_t> idx_to_randomize;
-
-  std::sample(A_index_lut.begin(), A_index_lut.end(), std::back_inserter(idx_to_randomize), num_randomized, std::mt19937{std::random_device{}()});
-
-  for (uint32_t idx : idx_to_randomize) {
-    uint32_t page_num, page_offset;
-    page_num = idx / PAGE_SIZE;
-    page_offset = idx % PAGE_SIZE;
-    
-
-    uint32_t rand_target_page_num = rand() % A_num_pages;
-
-    uint32_t target_idx = rand_target_page_num * (PAGE_SIZE / sizeof(float)) + page_offset;
-
-    // printf("idx: %d, page_num: %d, page_offset: %d target_page_num: %d target_idx: %d\n", idx, page_num, page_offset, rand_target_page_num, target_idx);
-
-    uint32_t tmp = 0;
-
-    if (target_idx > I*K) {
-      // printf("bad idx!\n");
-      // exit(1);
-    } else {
-
-      tmp = A_index_lut[target_idx];
-      A_index_lut[target_idx] = A_index_lut[idx];
-      A_index_lut[idx] = tmp;
-    }
   }
 
   std::vector<uint32_t> B_index_lut(J*K);
@@ -123,62 +128,8 @@ int main(int argc, char *argv[]) {
     B_index_lut[i] = i;
   }
 
-  idx_to_randomize.clear();
-
-  std::sample(B_index_lut.begin(), B_index_lut.end(), std::back_inserter(idx_to_randomize), num_randomized, std::mt19937{std::random_device{}()});
-
-  for (uint32_t idx : idx_to_randomize) {
-    uint32_t page_num, page_offset;
-    page_num = idx / PAGE_SIZE;
-    page_offset = idx % PAGE_SIZE;
-    
-
-    uint32_t rand_target_page_num = rand() % A_num_pages;
-
-    uint32_t target_idx = rand_target_page_num * (PAGE_SIZE / sizeof(float)) + page_offset;
-
-    // printf("idx: %d, page_num: %d, page_offset: %d target_page_num: %d target_idx: %d\n", idx, page_num, page_offset, rand_target_page_num, target_idx);
-
-    uint32_t tmp = 0;
-
-    if (target_idx > J*K) {
-      // printf("bad idx!\n");
-      // exit(1);
-    } else {
-
-      tmp = B_index_lut[target_idx];
-      B_index_lut[target_idx] = B_index_lut[idx];
-      B_index_lut[idx] = tmp;
-    }
-  }
-
-  // for (uint32_t idx : A_index_lut) {
-  //   printf("%d, ", idx);
-  // }
-
-  
-
-  // std::vector<uint32_t> A_p_index_order(C_num_pages);
-  // std::random_shuffle(A_p_index_order.begin(), A_p_index_order.end());
-
-  // unsigned int counter = 0;
-  // for (unsigned int p_offset = 0; p_offset < PAGE_SIZE; p_offset++) {
-  //   for (unsigned int p_idx = 0; p_idx < C_num_pages; p_idx++) {
-  //     unsigned int idx = A_p_index_order[p_idx] * PAGE_SIZE + p_offset;
-
-  //     if (counter < I*K) {
-  //       A_index_lut[counter] = idx;
-  //     }
-
-  //     counter++;
-  //   }
-  // }
-
-  // for (unsigned int i = 0; i < I*K; i++) {
-  //   printf("%x\n", A_index_lut[i]);
-  // }
-
-  // while (1) {};
+  randomly_swap_cache_blocks_across_pages(A_index_lut, frac_randomized);
+  randomly_swap_cache_blocks_across_pages(B_index_lut, frac_randomized);
 
 
   PerfProfiler myperf = get_perf_config(perf_command);
